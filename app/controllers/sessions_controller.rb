42604856d04bcaec607f0f3a62a26301496197ad
Melissa Vaiden <melissavaiden@gmail.com>
2023-01-24 20:11:02 -0500

    commit tree 23add00e210dc1fc8fab0af3b89a1478c137c8ce

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: 'Invalid Credentials' , status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        render json: 'No Content', status: :no_content
+    end
+end
